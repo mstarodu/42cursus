af739c04a9be41492791017c271b74c937776313
mstarodu <scrtx@icloud.com>
2023-11-16 01:26:35 +0100

    add blocks {}

--- Libft/1_solution/tester.c
+++ Libft/1_solution/tester.c
@@ -7,37 +7,40 @@
 
 int	main(void)
 {
-	/* ft_isalpha - ft0  */
+	{
 	printf("=== ft_isalpha ===\n");
-	const char test_ft0[] = {EOF, 34, 'd', 'S', -2, '-', 255, 28};
-	const int number_of_ft0 = sizeof(test_ft0)/sizeof(test_ft0[0]);
-	for (int i = 0; i < number_of_ft0; i++)
+	const char data[] = {EOF, 34, 'd', 'S', -2, '-', 255, 28};
+	const int size = sizeof(data)/sizeof(data[0]);
+	for (int i = 0; i < size; i++)
 		printf("var: %3i | ft: %i, std: %i - %s\n",
-				test_ft0[i],
-				ft_isalpha(test_ft0[i]),
-				isalpha(test_ft0[i]),
-				(ft_isalpha(test_ft0[i]) == isalpha(test_ft0[i])) ? "OK!" : "FAIL!");
+				data[i],
+				ft_isalpha(data[i]),
+				isalpha(data[i]),
+				(ft_isalpha(data[i]) == isalpha(data[i])) ? "OK!" : "FAIL!");
+	}
 
-	/* ft_isdigit - ft1  */
+	{
 	printf("=== ft_isdigit ===\n");
-	const char test_ft1[] = {1, '1', '0', 0, EOF, -1};
-	const int number_of_ft1 = sizeof(test_ft1)/sizeof(test_ft1[0]);
-	for (int i = 0; i < number_of_ft1; i++)
+	const char data[] = {1, '1', '0', 0, EOF, -1};
+	const int number = sizeof(data)/sizeof(data[0]);
+	for (int i = 0; i < number; i++)
 		printf("var: %3i | ft: %i, std: %i - %s\n",
-				test_ft1[i],
-				ft_isdigit(test_ft1[i]),
-				isdigit(test_ft1[i]),
-				(ft_isdigit(test_ft1[i]) == isdigit(test_ft1[i])) ? "OK!" : "FAIL!");
-	
-	/* ft_isalnum - ft2  */
+				data[i],
+				ft_isdigit(data[i]),
+				isdigit(data[i]),
+				(ft_isdigit(data[i]) == isdigit(data[i])) ? "OK!" : "FAIL!");
+	}
+
+	{
 	printf("=== ft_isalnum ===\n");
-	const char test_ft2[] = {1, '1', '0', 0, EOF, -1, EOF, 34, 'd', 'S', -2, '-', 255, 28};
-	const int number_of_ft2 = sizeof(test_ft2)/sizeof(test_ft2[0]);
-	for (int i = 0; i < number_of_ft2; i++)
+	const char data[] = {1, '1', '0', 0, EOF, -1, EOF, 34, 'd', 'S', -2, '-', 255, 28};
+	const int number = sizeof(data)/sizeof(data[0]);
+	for (int i = 0; i < number; i++)
 		printf("var: %3i | ft: %i, std: %i - %s\n",
-				test_ft2[i],
-				ft_isalnum(test_ft2[i]),
-				isalnum(test_ft2[i]),
-				(ft_isalnum(test_ft2[i]) == isalnum(test_ft2[i])) ? "OK!" : "FAIL!");
+				data[i],
+				ft_isalnum(data[i]),
+				isalnum(data[i]),
+				(ft_isalnum(data[i]) == isalnum(data[i])) ? "OK!" : "FAIL!");
+	}
 	return 0;
 }
