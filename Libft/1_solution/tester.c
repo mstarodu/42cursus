58cec7c5157b45663648b65f065ae0015f3c6159
mstarodu <scrtx@icloud.com>
2023-11-16 01:44:16 +0100

    isascii

--- Libft/1_solution/tester.c
+++ Libft/1_solution/tester.c
@@ -3,6 +3,7 @@
 #include "ft_isalpha.c"
 #include "ft_isdigit.c"
 #include "ft_isalnum.c"
+#include "ft_isascii.c"
 
 
 int	main(void)
@@ -41,6 +42,29 @@ int	main(void)
 				ft_isalnum(data[i]),
 				isalnum(data[i]),
 				(ft_isalnum(data[i]) == isalnum(data[i])) ? "OK!" : "FAIL!");
+	}
+
+	{
+	printf("=== ft_isascii ===\n");
+	const char data[] = {
+	    0,    // Null character
+	    'a',  // Lowercase ASCII
+	    'Z',  // Uppercase ASCII
+	    '0',  // Digit ASCII
+	    31,   // Control character (non-printable ASCII)
+	    127,  // Delete (non-printable ASCII)
+	    128,  // First non-ASCII character
+	    255,  // Last byte value (non-ASCII)
+	    -1,   // Negative value, typically represents non-ASCII
+	    200   // Another non-ASCII character
+	};
+	const int number = sizeof(data)/sizeof(data[0]);
+	for (int i = 0; i < number; i++)
+		printf("var: %3i | ft: %i, std: %i - %s\n",
+				data[i],
+				ft_isascii(data[i]),
+				isascii(data[i]),
+				(ft_isascii(data[i]) == isascii(data[i])) ? "OK!" : "FAIL!");
 	}
 	return 0;
 }
