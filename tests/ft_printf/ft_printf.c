09bd2b9016707a57a567e74fe28ceba34c385a94
myko <scrtx@icloud.com>
2024-02-04 18:22:13 +0100

    update tests for ft_printf

--- tests/ft_printf/ft_printf.c
+++ tests/ft_printf/ft_printf.c
@@ -1,4 +1,4 @@
-#include "../ft_printf/ft_printf.h"
+#include "../../ft_printf/ft_printf.h"
 #include <stdio.h>
 
 int	main(void)
@@ -10,10 +10,5 @@ int	main(void)
 	unsigned int	ui = 3243243;
 	int	std = printf("STD: Hello! Hi %%d%d(%%s%s)! You are 100%% cool! %%s%s - is not a number. Pointer has to point to %%p%p - %%u%u %%x%x %%X%X\n", d, five, str, &str, ui, dd, dd);
 	int	ft = ft_printf(" FT: Hello! Hi %%d%d(%%s%s)! You are 100%% cool! %%s%s - is not a number. Pointer has to point to %%p%p - %%u%u %%x%x %%X%X\n", d, five, str, &str, ui, dd, dd);
-	printf("%d - %d\n", std, ft);
-	printf("Nil printf: %p\n", NULL);
-	ft_printf("Nil\t ft_printf: %p\n", NULL);
-	printf("%i\n", printf("\001\002\007\v\010\f\r\n"));
-	ft_printf("%i\n", ft_printf("\001\002\007\v\010\f\r\n"));
-	printf("%zi\n", write(1, NULL, 1));
+	printf("%i  -- %i\n", std,  ft);
 }
