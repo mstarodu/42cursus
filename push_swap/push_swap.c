36d6643d956914701834892fe5f97a5a74298547
myko <scrtx@icloud.com>
2024-02-19 00:57:58 +0100

    st_split and h

--- push_swap/push_swap.c
+++ push_swap/push_swap.c
@@ -10,32 +10,7 @@
 /*                                                                            */
 /* ************************************************************************** */
 
-#include <stdlib.h>
-#include <unistd.h>
-#include <limits.h>
-#define DUMMY 0
-#define PLUS 1
-#define MINUS -1
-#define OK 0
-#define FAIL -1
-#define TRUE 1
-#define FALSE 0
-
-typedef int	t_result;
-typedef int	t_bool;
-typedef char*	t_string;
-
-typedef struct s_node
-{
-	int				nbr;
-	struct s_node	*next;
-}	t_node;
-
-typedef struct s_list
-{
-	t_node	*head;
-	t_node	*tail;
-}	t_list;
+#include  "push_swap.h"
 
 // Check is the list has duplicates
 
--- push_swap/push_swap.h
+++ push_swap/push_swap.h
@@ -6,19 +6,38 @@
 /*   By: mstarodu <mstarodu@student.42berlin.de>    +#+  +:+       +#+        */
 /*                                                +#+#+#+#+#+   +#+           */
 /*   Created: 2024/02/18 23:16:36 by mstarodu          #+#    #+#             */
-/*   Updated: 2024/02/18 23:21:03 by mstarodu         ###   ########.fr       */
+/*   Updated: 2024/02/19 00:51:22 by mstarodu         ###   ########.fr       */
 /*                                                                            */
 /* ************************************************************************** */
 
 #ifndef PUSH_SWAP_H
-#include <stdlib.h>
-#define IN 1
-#define OUT 0
-#define FAIL -1
-#define OK 0
-#define EOS 0
+# define PUSH_SWAP_H
+# include <stdlib.h>
+# include <unistd.h>
+# include <limits.h>
+# define DUMMY 0
+# define PLUS 1
+# define MINUS -1
+# define OK 0
+# define FAIL -1
+# define TRUE 1
+# define FALSE 0
+# define EOS 0
+# define IN 1
+# define OUT 0
 
-typedef char * t_string;
-typedef int t_result;
+typedef int		t_result;
+typedef int		t_bool;
+typedef char*	t_string;
+typedef struct s_node
+{
+	int				nbr;
+	struct s_node	*next;
+}	t_node;
+typedef struct s_list
+{
+	t_node	*head;
+	t_node	*tail;
+}	t_list;
 
 #endif
